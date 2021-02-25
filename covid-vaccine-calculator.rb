d36e97139a37fe35d452b0f7712bb017b9379ebf
Erik Thiem <erikthiem@gmail.com>
2021-02-24 23:24:15 -0500

    Add in ability to specify percentage of people who will get it

--- covid-vaccine-calculator.rb
+++ covid-vaccine-calculator.rb
@@ -5,6 +5,8 @@ require_relative 'lib/smart_prompt'
 
 state = SmartPrompt.get_input("Which state? Please use 2 letter code", "OH")
 age = SmartPrompt.get_input("How old?", 25).to_i
+percentage_of_people_who_will_get_it = SmartPrompt.get_input("What percentage of people do you think will get it when offered?", 70).to_f / 100
+
 bloomberg_data = BloombergParser.new
 census_data = CensusParser.new(state, age)
 
@@ -14,8 +16,6 @@ already_completed_vaccination = bloomberg_data.completed_vaccinations_in_state(s
 
 people_ahead_of_you = census_data.people_older_than_you_in_your_state - already_completed_vaccination
 
-percentage_of_people_who_will_get_it = 0.7
-
 people_ahead_of_you_who_will_get_it = people_ahead_of_you * percentage_of_people_who_will_get_it
 
 days_until_you_can_get_it = people_ahead_of_you_who_will_get_it / vaccinations_per_day_in_state_right_now
@@ -23,4 +23,4 @@ days_until_you_can_get_it = people_ahead_of_you_who_will_get_it / vaccinations_p
 day_you_can_get_it = Date.today + days_until_you_can_get_it
 
 
-puts "You can probably get the vaccine around #{day_you_can_get_it.strftime("%B %d, %Y")}"
+puts "\nYou can probably get the vaccine around #{day_you_can_get_it.strftime("%B %d, %Y")}"
